3943dd7b2437c4d81cc4b8b2f9801be1f0dba77b
Joseph Esmaail <josephesmaail93@gmail.com>
2017-05-03 22:29:46 +0100

    Working custom readline and getaccounts request

---
+++ Sharponzo.Logic/ApiLayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharponzo.Logic
+{
+    public class ApiLayer
+    {
+        private string _accessToken;
+        private readonly HttpClient _httpClient;
+
+        private const string BASE_URI = "https://api.monzo.com";
+
+        public ApiLayer(string accessToken)
+        {
+            _accessToken = accessToken;
+            _httpClient = new HttpClient{BaseAddress = new Uri(BASE_URI)};
+
+            if (_httpClient == null) throw new Exception();
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+        }
+
+        public async Task<string> GetAccounts()
+        {
+            var response = await _httpClient.GetAsync($"/accounts");
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception();
+            }
+
+            return body;
+        }
+    }
+}
--- Sharponzo.Logic/Program.cs
+++ Sharponzo.Logic/Program.cs
@@ -1,16 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Sharponzo.Logic
 {
     public class Program
     {
-        public static void main()
-        {
+        private const int READLINE_BUFFER_SIZE = 290;
 
+        public static void Main()
+        {
+            Console.WriteLine("Please enter Monzo Access Token: ");
+            var access = ReadLine();
+            var api = new ApiLayer(access);
+
+            var result = api.GetAccounts().Result;
         }
+
+
+        // Required as the Monzo Access Token is massive!
+        private static string ReadLine()
+        {
+            var inputStream = Console.OpenStandardInput(READLINE_BUFFER_SIZE);
+            var bytes = new byte[READLINE_BUFFER_SIZE];
+            var outputLength = inputStream.Read(bytes, 0, READLINE_BUFFER_SIZE);
+
+            // The Carriage return gets included in the resulting string, so needs to be removed
+            var result = Encoding.Default.GetString(bytes);
+            return result.Substring(0, result.Length - 2);
+        }
+
+        // Need to think about doing it as web app, Monzo requires a web kickback
+        //public static async Task<string> Authentication()
+        //{
+        //    var clientId = "";
+        //    var clientSecret = "";
+        //    var baseUrl = "";
+        //    var accessToken = "";
+
+        //    // GetAccessToken (async task)
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri(baseUrl);
+
+        //        // Set Response to be JSON
+        //        client.DefaultRequestHeaders.Accept.Clear();
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //        // Set data to POST
+        //        var dataToPost = new List<KeyValuePair<string, string>>();
+        //        dataToPost.Add(new KeyValuePair<string, string>("grantType", "client_credentials"));
+        //        dataToPost.Add(new KeyValuePair<string, string>("clientId", clientId));
+        //        dataToPost.Add(new KeyValuePair<string, string>("clientSecret", clientSecret));
+        //        var content = new FormUrlEncodedContent(dataToPost);
+                
+        //        // Post the data and get response
+        //        var response = await client.PostAsync("Token", content);
+        //        var jsonString = await response.Content.ReadAsStringAsync();
+        //        var responseData = JsonConvert.DeserializeObject(jsonString);
+
+        //        return ((dynamic) responseData).access_token;
+        //    }
+        //}
     }
 }
