3bd67ff763a1167116cf5a738fa52d7bf86f84eb
Joseph <josephesmaail93@gmail.com>
2018-08-19 23:54:17 +0100

    Project initiated on Linux

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sharponzo.Models;
+using RestSharp;
+using Sharponzo.Logic;
+
+namespace Sharponzo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void Monzo()
+        {
+            ViewData["Message"] = "Hello World";
+
+            var auth = new OAuth();
+            auth.Authenticate();
+            // return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Logic/OAuth.cs
@@ -0,0 +1,36 @@
+using RestSharp;
+
+namespace Sharponzo.Logic
+{
+    public class OAuth
+    {
+        private IRestClient _client;
+
+        private const string BASE_URI = "http://auth.monzo.com";
+
+        // This cannot be committed, needs to live on machine/confg
+        private const string CLIENT_ID = ""; 
+        
+        // This should also live in config
+        private const string REDIRECT_URI = "http://localhost:5000";
+
+        private const string RESPONSE_TYPE = "code";
+
+        public OAuth() => _client = new RestClient(BASE_URI);
+
+        public void Authenticate()
+        {
+            //"https://auth.monzo.com/?client_id=$client_id&redirect_uri=$redirect_uri&response_type=code&state=$state_token"
+
+            var request = new RestRequest();
+            request.AddParameter("client_id", CLIENT_ID);
+            request.AddParameter("redirect_uri", REDIRECT_URI);
+            request.AddParameter("response_type", RESPONSE_TYPE);
+
+            var response = _client.Execute(request);
+
+            var content = response.Content;
+        }
+
+    }
+}
